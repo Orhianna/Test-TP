91560fe9cdd47755ff3633c501e34faa8933d88b
nicolasburgueno <nicolasburgo12@hotmail.com>
2022-01-07 13:36:05 -0300

    se agrego client

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import Login from "./Components/Login";
+import React from "react";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
